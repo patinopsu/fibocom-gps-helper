66ddc35fe1d4256f1e1028bdd72a91e702737552
Patin Muangjan <patinopsu@proton.me>
2025-05-10 04:55:33 +0700

    Add Scripts

---
+++ fibocom-gps-resume.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for i in {1..60}; do
+    MODEM_PATH=$(mmcli -L 2>/dev/null | grep -o '/org/freedesktop/ModemManager1/Modem/[0-9]*' | tail -n1)
+    if [ -n "$MODEM_PATH" ]; then
+	echo "[INFO] Re-enabling GPS..."
+        echo "Modem found at $MODEM_PATH"
+        mmcli -m "$MODEM_PATH" --location-enable-gps-nmea --location-enable-gps-raw
+	echo "[INFO] Restarting gpsd..."
+	systemctl restart gpsd
+        exit 0
+    else
+        echo "Modem not found, waiting..."
+        sleep 1
+    fi
+done
+
+echo "Modem not found after 60 seconds. Giving up."
+exit 1
